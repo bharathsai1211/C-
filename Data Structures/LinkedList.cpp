28af3042d68166d88c78f7898e78097e934edb0e
bharatsai1211 <bharatsai1211@gamil.com>
2021-06-22 16:24:18 +0530

    deletetion method added

--- Data Structures/LinkedList.cpp
+++ Data Structures/LinkedList.cpp
@@ -22,64 +22,88 @@ void print()
 	cout << "List is: ";
 	while (temp != NULL)
 	{
-		cout << temp->data<<" ";
+		cout << temp->data << " ";
 		temp = temp->next;
 	}
-	cout<<"\n";
+	cout << "\n";
 }
 void insertAtPosition()
 {
-	int pos,x;
-	cout<<"Enter postion:\n";
-	cin>>pos;
-	cout<<"Enter the Value:\n";
-	cin>>x;
-	Node* temp=new Node();
-	temp->data=x;
-	temp->next=NULL;
-	if(pos==1)
+	int pos, x;
+	cout << "Enter postion:\n";
+	cin >> pos;
+	cout << "Enter the Value:\n";
+	cin >> x;
+	Node* temp = new Node();
+	temp->data = x;
+	temp->next = NULL;
+	if (pos == 1)
 	{
-		temp->next=head;
-		head=temp;
+		temp->next = head;
+		head = temp;
 		return;
 	}
-	Node *temp1=head;
-	for(int i=0;i<pos-2;i++)
+	Node *temp1 = head;
+	for (int i = 0; i < pos - 2; i++)
 	{
-		temp1=temp1->next;
+		temp1 = temp1->next;
 	}
-	temp->next=temp1->next;
-	temp1->next=temp;
+	temp->next = temp1->next;
+	temp1->next = temp;
+}
+void deleteAt()
+{
+	int pos;
+	cout << "Enter postion:\n";
+	cin >> pos;
+	Node* temp = head;
+	if (pos == 1)
+	{
+		head = temp->next;
+		free(temp);
+		return;
+	}
+	for (int i = 0; i < pos - 2; i++)
+	{
+		temp = temp->next;
+	}
+	Node* temp1 = temp->next;
+	temp->next = temp1->next;
+	free(temp1);
 }
 int main()
 {
 	head = NULL;
-	
-	while(true)
+	while (true)
 	{
-		cout<<"Enter 1 to Insert at Beg:\n";
-		cout<<"Enter 2 to Insert at position n:\n";
-		cout<<"Enter 3 Print Elements of List:\n";
+		cout << "Enter 1 to Insert at Beg:\n";
+		cout << "Enter 2 to Insert at position n:\n";
+		cout << "Enter 3 Print Elements of List:\n";
+		cout << "Enter 4 to delete Element at postition n:\n";
 		int value;
-		cin>>value;
-		switch(value)
+		cin >> value;
+		switch (value)
 		{
-			case 1:
-				inserAtBeg();
-				break;
-			case 2:
-				insertAtPosition();
-				break;
-			case 3:
-				print();
-				break;
-		}	
-		if(!(value>0&&value<=3))
+		case 1:
+			inserAtBeg();
+			break;
+		case 2:
+			insertAtPosition();
+			break;
+		case 3:
+			print();
+			break;
+		case 4:
+			deleteAt();
+			break;
+
+		}
+		if (!(value > 0 && value <= 4))
 		{
-			cout<<"Entered Wrong Option";
+			cout << "Entered Wrong Option";
 			break;
 		}
-		
+
 	}
-			
+
 }
